084f2f738758f5d30d8c12f74ac05f07661b7c1b
uglyflower <141611810+uglyflower@users.noreply.github.com>
2023-12-03 16:17:22 -0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'80315fe3-1c03-4fdb-9c86-ba9add456aee'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
